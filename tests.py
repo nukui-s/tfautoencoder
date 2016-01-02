0342a2cd3983a240b16f9e8cc2dff0a0a5d4c6de
Nukui <nukui.s.aa@gmail.com>
2016-01-02 18:31:56 +0900

    modiry test code

--- tests.py
+++ tests.py
@@ -9,12 +9,11 @@ from autoencoder import TFAutoEncoder
 class Test(tf.test.TestCase):
     def testAutoEncoder(self):
         with self.test_session() as sess:
-            encoder = TFAutoEncoder(target_dim=3, hidden_units=[5],
-                                                     num_cores=8, steps=50)
+            encoder = TFAutoEncoder(hidden_dim=3, steps=50)
             X = np.random.rand(50000).reshape(5000,10)
             encoder.fit(X)
             Y = encoder.encode(X)
-            X_ = encoder.reconstructe(X)
+            X_ = encoder.reconstruct(X)
             print(X)
             print(Y)
             print(X_)
