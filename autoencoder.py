d8c1b65677ef4953ed358552ffa636c27ad9b0da
Nukui <nukui.s.aa@gmail.com>
2016-01-03 21:08:25 +0900

    Write doc and license

--- autoencoder.py
+++ autoencoder.py
@@ -1,22 +1,60 @@
+"""Autoencoder on Tensorflow"""
+
+# Authors: Nukui Shun <nukui.s@ai.cs.titech.ac.jp>
+#Licencse: BSD 3 clause
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from six.moves import xrange
+
 import numpy as np
 import tensorflow as tf
-import sklearn.datasets as dt
 
 ACTIVATE_FUNC = {"relu": tf.nn.relu,
                                 "sigmoid": tf.nn.sigmoid,
                                 "softplus": tf.nn.softplus}
+
 OPTIMIZER = {"sgd": tf.train.GradientDescentOptimizer,
                         "adagrad": tf.train.AdagradOptimizer,
                         "adam": tf.train.AdamOptimizer}
 
 class TFAutoEncoder(object):
-    """class for Auto Encoder on Tensorflow"""
+    """class for Auto Encoder on Tensorflow
 
-    def __init__(self, hidden_dim, learning_rate=0.01, noising=True,
-                        noise_stddev=10e-3, w_stddev=0.1, steps=50,
-                        activate_func="relu", optimizer="adam",
-                        lambda_w=1, continue_training=False,
-                        logdir=None, num_cores=4):
+        Attributes (hidden_dim)
+        --------------------
+        hidden_dim : The number of units in hidden layer
+
+        learning_rate : Learning rate used in optimization
+
+        noising : If True fitting as denoising autoencoder(defalut is False)
+
+        noise_stddev : Only used when noising is True
+
+        activate_func : Selected in 'relu'(default) 'softplus' 'sigmoid'
+
+        optimizer : Selected in 'sgd' 'adagrad' 'adam'(default)
+
+        steps : The number of steps in optimization
+
+        w_stddev : Used in initializing weight variables
+
+        lambda_w : Penalty coefficient of regularization term
+
+        num_cores : The number of cores used in Tensorflow computation
+
+        logdir : Directory to export log
+
+        continue_training : If True keep the learned state at each time
+                                        performing fit()
+    """
+
+    def __init__(self, hidden_dim, learning_rate=0.01, noising=False,
+                        noise_stddev=10e-2, activate_func="relu", optimizer="adam",
+                        steps=50, w_stddev=0.1, lambda_w=1.0, num_cores=4,
+                        logdir=None, continue_training=False):
         if not activate_func in ACTIVATE_FUNC:
             raise ValueError("activate_func must be chosen of the following:"
                                     "`relu``sigmoid` `softplus`")
@@ -38,6 +76,7 @@ class TFAutoEncoder(object):
         self._initialized = False
 
     def fit(self, data):
+        """Optimize autoencoder"""
         data = np.array(data)
         shape = data.shape
         if len(shape) != 2:
@@ -46,14 +85,14 @@ class TFAutoEncoder(object):
 
         #setup computational graph if not initialized
         if not (self.continue_training and self._initialized):
-            self.setup_graph()
+            self._setup_graph()
         sess = self._session
 
         #setup summary writer for TensorBoard
         if self.logdir:
             writer = tf.train.SummaryWriter(self.logdir, sess.graph_def)
 
-        for step in range(self.steps):
+        for step in xrange(self.steps):
             feed_dict = self._get_feed_dict(data, self.noising)
             loss, summ, _ = sess.run([self._loss, self._summ, self._optimize],
                                                     feed_dict=feed_dict)
@@ -63,6 +102,7 @@ class TFAutoEncoder(object):
 
 
     def encode(self, data):
+        """Encode data by learned autoencoder """
         sess = self._session
         feed_dict = self._get_feed_dict(data, noising=False)
         encoded = sess.run(self._encoded,
@@ -70,6 +110,7 @@ class TFAutoEncoder(object):
         return encoded
 
     def reconstruct(self, data):
+        """Encode and decode input data"""
         sess = self._session
         feed_dict = self._get_feed_dict(data, noising=False)
         reconstructed = sess.run(self._reconstructed,
@@ -88,7 +129,8 @@ class TFAutoEncoder(object):
             feed_dict[self._noise] = zeros
         return feed_dict
 
-    def setup_graph(self):
+    def _setup_graph(self):
+        """Setup computation graph for training"""
         self._graph = tf.Graph()
         with self._graph.as_default():
             input_dim, hidden_dim = self.input_dim, self.hidden_dim
@@ -140,12 +182,14 @@ class TFAutoEncoder(object):
 
     @classmethod
     def _weight_variable(cls, shape, stddev):
+        """Generate weight matrix variable by normal distribution"""
         init = tf.truncated_normal(shape, stddev)
         w = tf.Variable(init)
         return w
 
     @classmethod
     def _bias_variable(cls, shape):
+        """Generate bias vector variable by ones"""
         #init = tf.truncated_normal(shape, stddev)
         init = tf.ones(shape)
         b = tf.Variable(init)
@@ -153,17 +197,20 @@ class TFAutoEncoder(object):
 
     @classmethod
     def _generate_noise(cls, shape, stddev):
+        """Generate noise for denoising autoencoder"""
         noise = np.random.normal(size=shape, scale=stddev)
         return noise
 
     @property
     def weight(self):
+        """Return weight as numpy array"""
         sess = self._session
         weight = self._W
         return sess.run(weight)
 
     @property
     def bias(self):
+        """Return bias as numpy array"""
         sess = self._session
         b, c = self._b, self._c
         b = sess.run(b)
--- stackedautoencoder.py
+++ stackedautoencoder.py
@@ -1,14 +1,52 @@
+"""Stacked Autoencoder on Tensorflow"""
+
+# Authors: Nukui Shun <nukui.s@ai.cs.titech.ac.jp>
+#Licencse: BSD 3 clause
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
 import os
+from six.moves import xrange
+
 import numpy as np
 import tensorflow as tf
+
 from autoencoder import TFAutoEncoder
+from autoencoder import ACTIVATE_FUNC
 
 class TFStackedAutoEncoder(object):
-    """class for Stacked Auto Encoder on Tensorflow"""
+    """Class for Stacked Auto Encoder on Tensorflow
 
-    def __init__(self, layer_units=[], learning_rate=0.1, noising=False,
+        Attributes (layer_units)
+        --------------------
+        layer_units : The number of units in each layer.
+
+        learning_rate : Learning rate used in optimization
+
+        noising : If True fitting as denoising autoencoder(defalut is False)
+
+        noise_stddev : Only used when noising is True
+
+        activate_func : Selected in 'relu'(default) 'softplus' 'sigmoid'
+
+        optimizer : Selected in 'sgd' 'adagrad' 'adam'(default)
+
+        steps : The number of steps in optimization
+
+        w_stddev : Used in initializing weight variables
+
+        lambda_w : Penalty coefficient of regularization term
+
+        num_cores : The number of cores used in Tensorflow computation
+
+        logdir : Directory to export log
+    """
+
+    def __init__(self, layer_units, learning_rate=0.01, noising=False,
                         noise_stddev=10e-2, activate_func="relu", optimizer="adam",
-                        num_cores=4, steps=50, w_stddev=0.1, lambda_w=1.0,
+                        steps=50, w_stddev=0.1, lambda_w=1.0, num_cores=4,
                         logdir=None):
         self.layer_units = layer_units
         self.layer_num = len(layer_units)
@@ -23,8 +61,8 @@ class TFStackedAutoEncoder(object):
         self.optimizer = optimizer
         self.logdir = logdir
 
-
     def fit(self, data):
+        """Optimize stacked autoencoder from the bottom layer"""
         data = np.array(data)
         shape = data.shape
         if len(shape) != 2:
@@ -34,8 +72,7 @@ class TFStackedAutoEncoder(object):
         outputs = [data]
         weights = []
         biases = []
-        for n in range(1, self.layer_num):
-            print(n)
+        for n in xrange(1, self.layer_num):
             input_n = outputs[n-1]
             hidden_dim = self.layer_units[n]
             output_n, W_n, b_n = self._partial_fit(input_n, hidden_dim, n)
@@ -44,11 +81,51 @@ class TFStackedAutoEncoder(object):
             biases.append(b_n)
         self.weights = weights
         self.biases = biases
+        self._setup_encode()
+
+    def encode(self, data, layer=-1):
+        """Encode data by learned stacked autoencoder """
+        sess = self._session
+        encoded = sess.run(self._outputs[layer],
+                                    feed_dict={self._input: data})
+        return encoded
+
+    def _setup_encode(self):
+        """Setup computation graph for encode """
+        self._graph = tf.Graph()
+        with self._graph.as_default():
+            self._input = X = tf.placeholder("float", shape=[None, self.layer_units[0]])
+
+            #setup data nodes for weights and biases
+            weights = []
+            biases = []
+            for w in self.weights:
+                weights.append(tf.constant(w))
+            for b in self.biases:
+                biases.append(b)
+
+            #define encoder: outputs[-1] is final result of encoding
+            self._outputs = outputs = [X]
+            activate_func = ACTIVATE_FUNC[self.activate_func]
+            for n in xrange(self.layer_num-1):
+                x = outputs[n]
+                w = weights[n]
+                b = biases[n]
+                output_n = activate_func(tf.matmul(x, w) + b)
+                outputs.append(output_n)
+
+            #create session
+            self._session = tf.Session(config=tf.ConfigProto(
+                                                    inter_op_parallelism_threads=self.num_cores,
+                                                    intra_op_parallelism_threads=self.num_cores))
+
+
 
     def _partial_fit(self, data, hidden_dim, layer_no):
+        """Optimize single autoencoder"""
         #make logdir for each layer
         if self.logdir:
-            logdir = self.logdir + "/" + str(layer_no)
+            logdir = self.logdir + "/layer" + str(layer_no)
             if not os.path.exists(logdir):
                 os.makedirs(logdir)
         else:
@@ -70,8 +147,3 @@ class TFStackedAutoEncoder(object):
         weight = ae.weight
         bias = ae.bias[0]
         return output, weight, bias
-
-if __name__ == '__main__':
-    sae = TFStackedAutoEncoder(layer_units=[10, 5, 5, 3], steps=10)
-    X = np.arange(50000).reshape(5000, 10)
-    sae.fit(X)
