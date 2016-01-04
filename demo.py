4c1a8190855bcf86ef26e92208077bb947dd5208
Nukui <nukui.s.aa@gmail.com>
2016-01-05 01:28:04 +0900

    Make package

--- demo.py
+++ demo.py
@@ -3,18 +3,19 @@
 import numpy as np
 import tensorflow as tf
 
-from autoencoder import TFAutoEncoder
-from stackedautoencoder import TFStackedAutoEncoder
+from tfautoencoder import TFAutoEncoder
+from tfautoencoder import TFStackedAutoEncoder
 
+#input: 20 dim 5000 data
 data = np.arange(100000).reshape(5000, 20)
 
-#20 dim -> 5 dim
+#basic autoencoder 20 dim -> 5 dim
 ae = TFAutoEncoder(hidden_dim=5)
 ae.fit(data)
 encoded = ae.encode(data)
 reconstructed = ae.reconstruct(data)
 
-#encode 20 dim -> 10 dim -> 3dim
+#stacked autoencoder 20 dim -> 10 dim -> 3dim
 sae = TFStackedAutoEncoder(layer_units=[20, 10, 3])
 sae.fit(data)
 encoded = sae.encode(data)
---
+++ tfautoencoder/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#encoding utf-8
+
+from .autoencoder import TFAutoEncoder
+from .stackedautoencoder import TFStackedAutoEncoder
+
+__all__ = ['TFAutoEncoder',
+           'TFStackedAutoEncoder']
